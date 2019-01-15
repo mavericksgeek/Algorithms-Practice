b63c83cbc92e57520376221d99f39e9c7102f8b8
Shubharaj <sarsekar@islander.tamucc.edu>
2019-01-15 01:40:00 -0600

    Updated with Hoare scheme

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,6 +1,8 @@
 /*
 Quicksort in CPP
-Using the last element for the sort criteria
+Using the 
+1.) last element for the sort criteria Lomuto's scheme
+2.) Hoare scheme
 */
 
 #include<iostream>
@@ -15,7 +17,7 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
-int partition(int arr[], int low, int high)
+int LomutoPartition(int arr[], int low, int high)
 {
     int pivot = arr[high];
     int i = low - 1;
@@ -31,6 +33,26 @@ int partition(int arr[], int low, int high)
     return (i+1);
 }
 
+int HoarePartition(int arr[], int low, int high)
+{
+    int pivot = arr[(low + high) / 2];
+    int i = low - 1;
+    int j = high + 1;
+    while(1)
+    {
+        do{
+            i++;
+        }while(arr[i] < pivot);
+        do{
+            j--;
+        }while(arr[j] > pivot);
+        if(i >= j)
+         break;
+        swap(&arr[i], &arr[j]);
+    }
+    return j;  
+}
+
 void printarray(int arr[], int size)
 {
     for(int i = 0; i < size; i++)
@@ -40,21 +62,45 @@ void printarray(int arr[], int size)
     cout << "\n";
 }
 
-void quicksort(int arr[],int low, int high)
+void quicksortL(int arr[],int low, int high)
 {
     if(low < high)
     {
-        int pi = partition(arr, low, high);      
-        quicksort(arr, low, pi - 1);
-        quicksort(arr, pi+1, high);
+        int pi = LomutoPartition(arr, low, high);      
+        quicksortL(arr, low, pi - 1);
+        quicksortL(arr, pi+1, high);
+    }
+}
+
+void quicksortH(int arr[],int low, int high)
+{
+    if(low < high)
+    {
+        int pi = HoarePartition(arr, low, high);      
+        quicksortH(arr, low, pi);
+        quicksortH(arr, pi+1, high);
     }
 }
 
 int main()
 {
-    int arr[5] = {5,4,3,2,1};
-    int size = 5; 
-    
-    quicksort(arr, 0, size - 1);
-    printarray(arr, size);
+    //int arr[5] = {5,4,3,2,1};   Test case to check if the code is running
+    //int size = 5; 
+    cout << "Enter the size of the array\n";
+    cin >> size;
+    int arr[size];
+    cout <<"Enter the elements\n";
+    for(int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    int choice;
+    cout << "Select the type of Quick Sort \n1.) Lomuto scheme (last element as the pivot)\n2.)";
+    cout << "Hoare Scheme (Median of high and low as pivot\n";
+    cin >> choice;
+    if (choice == 1) 
+        quicksortL(arr, 0, size - 1);
+    else
+        quicksortH(arr, 0, size - 1);
+        printarray(arr, size);
 }
