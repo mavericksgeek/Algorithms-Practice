873f66c23299e138a814bae82ad5f113fdbfe237
Shubharaj <shubharsekar@gmail.com>
2019-01-15 16:02:15 -0600

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,3 +1,4 @@
+
 /* 
 Bubble sort in cpp
 */
@@ -8,5 +9,15 @@ using namespace std;
 
 int main()
 {
-
+  int arr[5] = {5,4,3,2,1};
+  int size = 5;
+  for(int i = 0 ; i < size; i++)
+  {
+    for(int j = 0; j < size; j++)
+    {
+      if(arr[j] > arr[j + 1])
+        swap(&arr[j], &arr[j + 1]);
+    }
+  }
+  printarray(arr,size);
 }
